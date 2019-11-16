9855aa6cd1325b94a35f5af883c5dc77fcb919c0
orshemesh <chikos2011@gmail.com>
2019-11-16 16:21:55 +0200

    Connect and Manager Login

---
+++ app/src/main/java/com/example/married_at_first_sight/login_maanger.java
@@ -0,0 +1,14 @@
+package com.example.married_at_first_sight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_maanger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_maanger);
+    }
+}
