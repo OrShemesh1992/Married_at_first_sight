38539f9ab93f7b3842ff61f53336037e6f21d4d0
orshemesh <chikos2011@gmail.com>
2019-11-17 15:32:09 +0200

    Connect and Manager Login

--- app/src/main/java/com/example/married_at_first_sight/questions.java
+++ app/src/main/java/com/example/married_at_first_sight/questions.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class login_maanger extends AppCompatActivity {
+public class questions extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_maanger);
+        setContentView(R.layout.activity_questions);
     }
 }
